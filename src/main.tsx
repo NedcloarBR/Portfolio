e7eea3305c0bc2c07a05e607b8fc90aa6f68aae7
NedcloarBR <nedcloar1@hotmail.com>
2024-03-04 16:24:20 -0300

    chore: remove unnecessary "/node_modules/" in flag-icons import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import "flag-icons/css/flag-icons.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
 import { RouterProvider } from "react-router-dom";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
 import MainContainer from "./components/MainContainer";
 import { ThemeProvider } from "./components/ThemeProvider";
 import i18n from "./lib/18n";
