beb5dd04815d8d89fec97eb8eef689d67f155aef
NedcloarBR <nedcloar1@hotmail.com>
2024-07-14 20:46:34 -0300

    feat(about): initial layout

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,14 +1,21 @@
 import { useTranslations } from "next-intl";
 
 export function About() {
-	const t = useTranslations();
+	const t = useTranslations("About");
 
 	return (
 		<section
 			id="about"
-			className="flex justify-center mt-8 h-screen text-4xl text-blue-700"
+			className="flex justify-center mt-8 h-screen text-white"
 		>
-			<div>{t("Hello")}</div>
+			<div className="flex items-center mt-8 overflow-wrap break-word">
+				<img
+					src="PPF.jpeg"
+					alt="Miguel Alexandre Uhlein PPF"
+					className="size-80 mt-4 inline-block mr-4 rounded-xl"
+				/>
+				<p className="max-w-md">{t("Text")}</p>
+			</div>
 		</section>
 	);
 }
