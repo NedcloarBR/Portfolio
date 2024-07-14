b3e2bc24f18f2a13f6bf146756813638417ee4de
NedcloarBR <nedcloar1@hotmail.com>
2024-07-14 19:35:34 -0300

    fix: Moon not fixed in button

--- src/components/theme/toggle-theme.tsx
+++ src/components/theme/toggle-theme.tsx
@@ -20,7 +20,11 @@ export function ToggleTheme() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="sm">
+				<Button
+					variant="outline"
+					size="sm"
+					className="relative flex items-center"
+				>
 					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">{}</span>
