78c84cc3e49a097a6a8fb8008e1c60e3d3074cf8
NedcloarBR <nedcloar1@hotmail.com>
2024-07-16 20:55:43 -0300

    feat: add WIP message

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,16 +1,17 @@
 import { useTranslations } from "next-intl";
 
 export function Start() {
-	const t = useTranslations("Start");
+	const t = useTranslations();
 
 	return (
 		<section
 			id="start"
 			className="h-screen--100px bg-zinc-600 flex flex-col items-center justify-center text-center space-y-2"
 		>
-			<div className="text-3xl">{t("Hello")}</div>
-			<div className="text-6xl">{t("Name")}</div>
-			<div className="text-2xl">{t("Details")}</div>
+      <h1>{t("WIP")}</h1>
+			<div className="text-3xl">{t("Start.Hello")}</div>
+			<div className="text-6xl">{t("Start.Name")}</div>
+			<div className="text-2xl">{t("Start.Details")}</div>
 		</section>
 	);
 }
