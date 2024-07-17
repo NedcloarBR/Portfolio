80e8c07b489191f208049bdcce7690299fdd6ba3
NedcloarBR <nedcloar1@hotmail.com>
2024-07-17 18:25:31 -0300

    fix: try to centralize Anchors in Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,20 +4,21 @@ export function Header() {
 	return (
     <section
 			id="header"
-			className="fixed w-screen h-[100px] space-x-4 flex bg-zinc-400 dark:bg-zinc-900 border-b-4 border-green-700"
+			className="fixed w-screen h-[100px] flex bg-zinc-400 dark:bg-zinc-900 border-b-4 border-green-700"
 		>
-			<nav className="px-4 gap-2 flex items-center w-full">
-        <div className="gap-4 flex flex-auto justify-center items-center">
+			<nav className="px-4 flex items-center justify-between w-full">
+        <div className="flex-1"></div>
+        <div className="flex justify-center items-center w-full gap-4 ml-28">
           <HeaderAnchor target="start"/>
           <HeaderAnchor target="about"/>
           <HeaderAnchor target="competencies"/>
           <HeaderAnchor target="projects"/>
           <HeaderAnchor target="contact"/>
         </div>
-				<div className="ml-auto flex gap-2 mr-8">
-					<ToggleTheme />
-					<ChangeLanguage />
-				</div>
+        <div className="flex gap-2 mr-8">
+				  <ToggleTheme />
+				  <ChangeLanguage />
+			  </div>
 			</nav>
 		</section>
 	);
