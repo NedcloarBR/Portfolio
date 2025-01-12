4e4441139360fbb2bea8b4246a268a13a8dca9ff
NedcloarBR <nedcloar1@hotmail.com>
2025-01-12 19:07:28 -0300

    fix: images api

--- src/app/api/images/[project]/route.ts
+++ src/app/api/images/[project]/route.ts
@@ -5,9 +5,9 @@ import type { NextRequest } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { project: string } }
+  context: { params: Promise<{ project: string }> }
 ) {
-  const { project } = await context.params;
+  const project = (await context.params).project;
 
   if (!project) {
     return NextResponse.json(
