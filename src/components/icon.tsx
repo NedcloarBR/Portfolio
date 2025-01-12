63124d3118c42d7a6b91e9991c85c7304e9ac6c6
NedcloarBR <nedcloar1@hotmail.com>
2025-01-12 12:51:09 -0300

    feat: Icon now accepts png via prop

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,9 +1,12 @@
 import { ComponentProps } from "react";
 
-interface Props extends ComponentProps<"img"> {
+interface IconProps extends ComponentProps<"img"> {
   name: string;
+  className?: string;
+  extension?: "svg" | "png";
 }
 
-export function Icon({ name, className }: Readonly<Props>) {
-  return <img className={className} src={`/icons/${name}.svg`} alt={`${name} Icon`} />
+export function Icon({ name, className, extension = "svg" }: Readonly<IconProps>) {
+  const iconSrc = `/icons/${name}.${extension}`;
+  return <img className={className} src={iconSrc} alt={`${name} Icon`} />;
 }
