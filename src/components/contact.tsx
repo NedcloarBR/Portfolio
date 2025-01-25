88f8c55b0f6245a86df0eb5124f48c8d5e7160d1
NedcloarBR <nedcloar1@hotmail.com>
2025-01-25 20:00:25 -0300

    feat: add initial email contact form

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,115 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Button,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  Textarea,
+} from "@/components/ui";
+
 export function Contact() {
-	return <section id="contact" className="h-screen"></section>;
+  const t = useTranslations("Contact");
+
+  const formSchema = z.object({
+    name: z
+      .string()
+      .min(3, {
+        message: t("Form.NameTooShort"),
+      })
+      .nonempty({}),
+    subject: z.string().nonempty(),
+    message: z.string().nonempty(),
+  });
+
+  type formType = z.infer<typeof formSchema>;
+
+  const form = useForm<formType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      subject: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: formType) {
+    const { name, subject, message } = values;
+
+    const body = `Name: ${name}\n\nMessage: ${message}`;
+
+    const mailtoLink = `mailto:nedcloar1@hotmail.com?subject=${subject}&body=${body}`;
+    window.open(mailtoLink, "_blank");
+  }
+
+  return (
+    <section id="contact" className="grid justify-center h-screen">
+      <h1 className="mt-8 flex justify-center items-center text-4xl">
+        {t("Title")}
+      </h1>
+      <div className="flex justify-center items-center gap-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("Form.Name")}</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>{t("Form.NameDescription")}</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="subject"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("Form.Subject")}</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    {t("Form.SubjectDescription")}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="message"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("Form.Message")}</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    {t("Form.MessageDescription")}
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </section>
+  );
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,8 @@ export * from "./card";
 export * from "./carousel";
 export * from "./dialog";
 export * from "./dropdown-menu";
+export * from "./form";
+export * from "./input";
+export * from "./textarea";
 export * from "./separator";
 export * from "./sheet";
